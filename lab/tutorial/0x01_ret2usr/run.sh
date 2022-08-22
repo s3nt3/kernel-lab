763fd0957eb14e8c0c09ca25995da9c019b25cb6
Zengxian Ding <zengxian.thomas@gmail.com>
2022-08-22 20:31:15 +0800

    tutorial: disable kvm for kernel debugging

--- lab/tutorial/0x01_ret2usr/run.sh
+++ lab/tutorial/0x01_ret2usr/run.sh
@@ -8,7 +8,7 @@ KERNEL_VERSION=${KERNEL_VERSION:-5.10.133}
 KERNEL_DIR=/root/.cache/kernel-lab/kernel/v${KERNEL_VERSION:0:1}.x/linux-${KERNEL_VERSION}
 
 qemu-system-x86_64 -s -S \
-    -enable-kvm -cpu kvm64 \
+    -cpu qemu64 \
     -smp 2,cores=2,threads=1 -m 2048 \
     -drive format=qcow2,file=${DEBIAN_DIR}/debian.qcow2 \
     -kernel ${KERNEL_DIR}/arch/x86/boot/bzImage \
